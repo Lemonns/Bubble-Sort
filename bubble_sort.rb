8440dee76f837ae253affe90edb46b8cfedb7fec
Trey <104473511+Lemonns@users.noreply.github.com>
2022-08-04 03:48:58 -0500

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,9 @@
 def bubble_sort(array)
     for i in 1..array.length - 1
-        sorted = true
         for j in 0..array.length - 1
             if array[j] > array[i]
-                sorted = false
                 array[j], array[i] = array[i], array[j]
             end
-        end
-        if sorted
-            break
         end
     end
     array
